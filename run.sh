857de8b05228f59410601e461016ce6813666f15
Ansel Halliburton <github@anseljh.com>
2016-09-09 10:27:35 -0700

    Update run script
    Don't need CL API token after all; use correct Python invocation

--- run.sh
+++ run.sh
@@ -1,6 +1,2 @@
 export SLACKBOT_API_TOKEN=YOUR_BOT_API_TOKEN
-# Get a CourtListener REST API token:
-# https://www.courtlistener.com/api/rest-info/
-export COURTLISTENER_API_TOKEN=YOUR_COURTLISTENER_API_TOKEN
-casebot
-
+python3 casebot.py
