7704ca5e4063e976e494bff2901e7996f5498b2b
Ansel Halliburton <github@anseljh.com>
2016-09-09 00:23:45 -0700

    Add script for setting environment variables and running bot

---
+++ run.sh
@@ -0,0 +1,6 @@
+export SLACKBOT_API_TOKEN=YOUR_BOT_API_TOKEN
+# Get a CourtListener REST API token:
+# https://www.courtlistener.com/api/rest-info/
+export COURTLISTENER_API_TOKEN=YOUR_COURTLISTENER_API_TOKEN
+casebot
+
