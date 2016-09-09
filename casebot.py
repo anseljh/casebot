016ae3bb379958f1ef00d65b7422592fa92fbdac
Ansel Halliburton <github@anseljh.com>
2016-09-09 01:12:35 -0700

    Stub

--- casebot.py
+++ casebot.py
@@ -1,14 +1,21 @@
 from slackbot.bot import Bot
+from slackbot import settings
+from slackbot.bot import listen_to, respond_to
 
 
-@listen_to('yo')
-def yo(message):
-    message.reply('Yo ho ho!')
+settings.DEFAULT_REPLY = "Eh?"
+
+
+@respond_to("123 F.2d 456")
+def handle_citation(message):
+    message.reply("Full citation: Example v. Example, 123 F.2d 456 (9th Cir. 2000)")
+    message.reply("CourtListener URL: https://stuff")
 
 
 def main():
     bot = Bot()
     bot.run()
+
 
 if __name__ == "__main__":
     main()
