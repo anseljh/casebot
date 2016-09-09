3f8874461d4bb71afb64ed20d1781c33c098f25e
Ansel Halliburton <github@anseljh.com>
2016-09-09 10:25:39 -0700

    It works!

--- casebot.py
+++ casebot.py
@@ -1,15 +1,33 @@
 from slackbot.bot import Bot
 from slackbot import settings
 from slackbot.bot import listen_to, respond_to
+from string import Template
+import requests
+from pprint import pprint
 
 
 settings.DEFAULT_REPLY = "Eh?"
+CL_URL_TEMPLATE = Template("https://www.courtlistener.com/c/$reporter/$volume/$page/")
+MINIMUM_VIABLE_CITATION_PATTERN = r"^(\d+)\s([A-Za-z0-9.]+)\s(\d+)$"
+USER_AGENT_STRING = "casebot https://github.com/anseljh/casebot"
 
 
-@respond_to("123 F.2d 456")
-def handle_citation(message):
-    message.reply("Full citation: Example v. Example, 123 F.2d 456 (9th Cir. 2000)")
-    message.reply("CourtListener URL: https://stuff")
+@respond_to(MINIMUM_VIABLE_CITATION_PATTERN)
+def handle_citation(message, volume=None, reporter=None, page=None):
+    # Look up using CourtListener /c tool
+    mapping = {'volume': volume, 'reporter': reporter, 'page': page}
+    url = CL_URL_TEMPLATE.substitute(mapping)
+    request_headers = {'user-agent': USER_AGENT_STRING}
+    response = requests.get(url, headers=request_headers)
+
+    # Give some output on stdout
+    print(response.status_code)
+    pprint(response.headers)
+    print(response.url)
+    print(response)
+
+    # Send the message!
+    message.reply(response.url)
 
 
 def main():
